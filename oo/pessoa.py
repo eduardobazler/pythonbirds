d16a0ec5dada421232d09bfe804581145683e112
eduardobazler <eduardo.bazler@unesp.br>
2021-04-03 12:14:05 -0300

    Criados atributos de intância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,15 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 23):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
     p = Pessoa()
+    p.nome = 'eduardo'
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
