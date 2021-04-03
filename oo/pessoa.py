16fea9318d1265775abae4b5135de72b8d3799a5
eduardobazler <eduardo.bazler@unesp.br>
2021-04-03 16:23:33 -0300

    Criado atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 23):  #atibutos de instância
         self.nome = nome
         self.idade = idade
@@ -19,3 +21,9 @@ if __name__ == '__main__':
     print(eduardo.__dict__)
     del eduardo.idade
     print(eduardo.__dict__)
+    eduardo.olhos = 3  #esse valor só será trocado no objeto Eduardo, porém ela só está criado no __init__
+    Pessoa.olhos = 4 #agora, quando eu troco na classe, todos os objetos, exceto eduardo, terá o valor de 4
+    print(Pessoa.olhos)
+    print(eduardo.olhos)
+    print(eduardo.__dict__)
+    print(id(Pessoa.olhos), id(eduardo.olhos))
